bb0efa5871c5d3b7f01002e004bd59fd3f8189a0
Bach Ngoc Toan <ngoctoan@live.com>
2018-12-28 08:35:43 +0700

    Function API

---
+++ WebAPICoreDapper/Controllers/FunctionController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebAPICoreDapper.Dtos;
+using WebAPICoreDapper.Filters;
+using WebAPICoreDapper.Models;
+
+namespace WebAPICoreDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FunctionController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public FunctionController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DbConnectionString");
+        }
+
+        // GET: api/Role
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    await conn.OpenAsync();
+
+                var paramaters = new DynamicParameters();
+                var result = await conn.QueryAsync<Function>("Get_Function_All", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+                return Ok(result);
+            }
+        }
+
+        // GET: api/Role/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var paramaters = new DynamicParameters();
+                paramaters.Add("@id", id);
+
+                var result = await conn.QueryAsync<Function>("Get_Function_ById", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+                return Ok(result.Single());
+            }
+        }
+
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetPaging(string keyword, int pageIndex, int pageSize)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    await conn.OpenAsync();
+
+                var paramaters = new DynamicParameters();
+                paramaters.Add("@keyword", keyword);
+                paramaters.Add("@pageIndex", pageIndex);
+                paramaters.Add("@pageSize", pageSize);
+                paramaters.Add("@totalRow", dbType: System.Data.DbType.Int32, direction: System.Data.ParameterDirection.Output);
+
+                var result = await conn.QueryAsync<Function>("Get_Function_AllPaging", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+
+                int totalRow = paramaters.Get<int>("@totalRow");
+
+                var pagedResult = new PagedResult<Function>()
+                {
+                    Items = result.ToList(),
+                    TotalRow = totalRow,
+                    PageIndex = pageIndex,
+                    PageSize = pageSize
+                };
+                return Ok(pagedResult);
+            }
+
+
+        }
+
+        // POST: api/Role
+        [HttpPost]
+        [ValidateModel]
+        public async Task<IActionResult> Post([FromBody] Function function)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var paramaters = new DynamicParameters();
+                paramaters.Add("@id", function.Id);
+                paramaters.Add("@name", function.Name);
+                paramaters.Add("@url", function.Url);
+                paramaters.Add("@parentId", function.ParentId);
+                paramaters.Add("@sortOrder", function.SortOrder);
+                paramaters.Add("@cssClass", function.CssClass);
+                paramaters.Add("@isActive", function.IsActive);
+                await conn.ExecuteAsync("Create_Function", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+                return Ok();
+            }
+        }
+
+        // PUT: api/Role/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([Required]Guid id, [FromBody] Function function)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var paramaters = new DynamicParameters();
+                paramaters.Add("@id", function.Id);
+                paramaters.Add("@name", function.Name);
+                paramaters.Add("@url", function.Url);
+                paramaters.Add("@parentId", function.ParentId);
+                paramaters.Add("@sortOrder", function.SortOrder);
+                paramaters.Add("@cssClass", function.CssClass);
+                paramaters.Add("@isActive", function.IsActive);
+
+                await conn.ExecuteAsync("Update_Function", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+                return Ok();
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var paramaters = new DynamicParameters();
+                paramaters.Add("@id", id);
+                await conn.ExecuteAsync("Delete_Function_ById", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+                return Ok();
+            }
+        }
+    }
+}
---
+++ WebAPICoreDapper/Models/Function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICoreDapper.Models
+{
+    public class Function
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+        public string ParentId { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public string CssClass { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
