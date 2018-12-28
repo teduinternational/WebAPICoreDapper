94fc4adc8724fa7b474c48b5687dc2376bcb639b
Bach Ngoc Toan <ngoctoan@live.com>
2018-12-28 08:17:08 +0700

    User API

---
+++ WebAPICoreDapper/Controllers/UserController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebAPICoreDapper.Dtos;
+using WebAPICoreDapper.Filters;
+using WebAPICoreDapper.Models;
+
+namespace WebAPICoreDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly string _connectionString;
+
+        public UserController(UserManager<AppUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _connectionString = configuration.GetConnectionString("DbConnectionString");
+        }
+
+        // GET: api/Role
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    await conn.OpenAsync();
+
+                var paramaters = new DynamicParameters();
+                var result = await conn.QueryAsync<AppUser>("Get_User_All", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+                return Ok(result);
+            }
+        }
+
+        // GET: api/Role/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await _userManager.FindByIdAsync(id));
+        }
+
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetPaging(string keyword, int pageIndex, int pageSize)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    await conn.OpenAsync();
+
+                var paramaters = new DynamicParameters();
+                paramaters.Add("@keyword", keyword);
+                paramaters.Add("@pageIndex", pageIndex);
+                paramaters.Add("@pageSize", pageSize);
+                paramaters.Add("@totalRow", dbType: System.Data.DbType.Int32, direction: System.Data.ParameterDirection.Output);
+
+                var result = await conn.QueryAsync<AppUser>("Get_User_AllPaging", paramaters, null, null, System.Data.CommandType.StoredProcedure);
+
+                int totalRow = paramaters.Get<int>("@totalRow");
+
+                var pagedResult = new PagedResult<AppUser>()
+                {
+                    Items = result.ToList(),
+                    TotalRow = totalRow,
+                    PageIndex = pageIndex,
+                    PageSize = pageSize
+                };
+                return Ok(pagedResult);
+            }
+
+
+        }
+
+        // POST: api/Role
+        [HttpPost]
+        [ValidateModel]
+        public async Task<IActionResult> Post([FromBody] AppUser user)
+        {
+            var result = await _userManager.CreateAsync(user);
+            if (result.Succeeded)
+                return Ok();
+            return BadRequest();
+        }
+
+        // PUT: api/Role/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([Required]Guid id, [FromBody] AppUser user)
+        {
+            user.Id = id;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+                return Ok();
+            return BadRequest();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+                return Ok();
+            return BadRequest();
+        }
+    }
+}
